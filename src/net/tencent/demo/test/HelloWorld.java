3cc37fd6b54612477af5a8f89d6d8caca6253db6
yanbo0039 <yanbo0039@yeah.net>
2019-07-01 14:42:30 +0800

    Java块

--- src/net/tencent/demo/test/HelloWorld.java
+++ src/net/tencent/demo/test/HelloWorld.java
@@ -1,6 +1,20 @@
 package net.tencent.demo.test;
 
 public class HelloWorld {
+	
+	{
+		for (int i = 0; i < 10; i++) {
+			if( i % 2 == 0) {
+				System.out.println("我是偶数: " + i);
+			}
+		}
+	}
+	
+	static {
+		if(true) {
+			System.out.println("true");
+		}
+	}
 
 	//main alt ---> / 自动生成
 	public static void main(String[] args) {
