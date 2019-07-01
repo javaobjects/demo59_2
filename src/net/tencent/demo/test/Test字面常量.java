333a86ee816def446f38a938c1835e762b64d382
yanbo0039 <yanbo0039@yeah.net>
2019-07-01 17:11:02 +0800

    Java-Jdk7对数据常量的支持

--- src/net/tencent/demo/test/Test字面常量.java
+++ src/net/tencent/demo/test/Test字面常量.java
@@ -24,6 +24,16 @@ public class Test字面常量 {
 		
 		System.out.println("g:" +g);
 		System.out.println("h:" +h);
+		
+		long money = 1_000_000_000;//可以三位一断也可以两位一断也可以一位一断，根据自己习惯
+		
+		//jdk7以后对这些数值常量的支持：可以使用下划线，增强可读性
+		int age = 0B10_10_10_10;//0b/0B都行 0b/0B 开头表示二进制 binary:二进制
+		int age2 = 0b1000000;
+		
+		System.out.println("age: " + age);//170
+		System.out.println("age2: " + age2);//64
+		
 	}
 
 }
