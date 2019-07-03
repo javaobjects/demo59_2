4ee16786aff1f975738b8f41e22d391b40423fea
yanbo0039 <yanbo0039@yeah.net>
2019-07-03 10:15:59 +0800

    数组的声明及初始化

---
+++ src/net/tencent/demo/test/TestArray1.java
@@ -0,0 +1,23 @@
+package net.tencent.demo.test;
+
+public class TestArray1 {
+
+	public static void main(String[] args) {
+
+		//声明一个变量a，整数数组类型
+		int[] a = {12,23,44,55,66};
+		
+		String[] b = {"abc","124","666"};
+		
+		double[] c = {12.34,45.66,77.890,99};
+		
+		char[] d = {'中','a','\n','\u1234',123};
+		
+		Student stu1 = new Student();
+		
+		Student stu2 = new Student();
+		
+		Student[] e = {stu1,stu2};
+	}
+
+}
