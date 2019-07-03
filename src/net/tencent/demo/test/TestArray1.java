25502e32e2e453ef7af60ee2b6dec13361ea96d3
yanbo0039 <yanbo0039@yeah.net>
2019-07-03 10:48:24 +0800

    数据的分类一维数组&二给数组&内存运行原理

--- src/net/tencent/demo/test/TestArray1.java
+++ src/net/tencent/demo/test/TestArray1.java
@@ -6,7 +6,7 @@ public class TestArray1 {
 
 		//声明一个变量a，整数数组类型
 		int[] a = {12,23,44,55,66};
-		
+		//声明一个变量b，String数组类型
 		String[] b = {"abc","124","666"};
 		
 		double[] c = {12.34,45.66,77.890,99};
@@ -18,6 +18,19 @@ public class TestArray1 {
 		Student stu2 = new Student();
 		
 		Student[] e = {stu1,stu2};
+		
+		
+		/**
+		 * 1,2,3
+		 * 4,5,6
+		 * 7,8,9
+		 * 
+		 * 56,67,345
+		 */
+		int[] k = {56,67,345};//一维数组
+		
+		int[][] y = {{1,2,3},{4,5,6},{7,8,9}};//多维数组
+		
 	}
 
 }
